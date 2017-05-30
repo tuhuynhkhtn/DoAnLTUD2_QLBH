4fe18a0cdda1b94c3d533241ef06bc0f58d549c1
TuHuynh <tuhuynhkhtn@gmail.com>
2017-05-30 19:08:12 +0700

    Demo tạo Layout của thầy
    Demo tạo Layout của thầy

---
+++ DemoDoAn/TaoLayout/Ultilities/Commands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaoLayout.Commands;
+
+namespace TaoLayout.Ultilities
+{
+    public class Commands
+    {
+        public static LeftMenuCmd LeftMenuCmd { get; }
+
+        static Commands()
+        {
+            LeftMenuCmd = new LeftMenuCmd();
+        }
+    }
+}
