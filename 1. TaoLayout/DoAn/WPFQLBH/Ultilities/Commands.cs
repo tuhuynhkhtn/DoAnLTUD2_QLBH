81a041f875b1e03e81884339c8f84ab5721774cd
TuHuynh <tuhuynhkhtn@gmail.com>
2017-05-30 19:12:52 +0700

    Tạo Layout
    Tạo Layout

---
+++ 1. TaoLayout/DoAn/WPFQLBH/Ultilities/Commands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFQLBH.Commands;
+
+namespace WPFQLBH.Ultilities
+{
+    public class Commands
+    {
+        public static LeftMenuCmd LeftMenuCmd { get; }
+
+        static Commands()
+        {
+            LeftMenuCmd = new LeftMenuCmd();
+        }
+    }
+}
